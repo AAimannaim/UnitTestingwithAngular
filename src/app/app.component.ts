13aeb2be6e00e2495a36d45f114e25409419dbd1
Aiman Naim Ariffin <aimannaim.ariffin@petronas.com>
2023-11-15 15:13:52 +0800

    Add the karma config files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  triggerStudentAdd() {
+    throw new Error('Method not implemented.');
+  }
   title = 'Testing';
+  studentName: string | undefined;
 }
